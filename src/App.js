47b120c29075d93f4853c889b058155597521e0b
xjh <xjhit28@163.com>
2023-04-27 16:21:00 +0800

    20230427

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
-//import logo from './logo.svg';
 import './App.css';
-//import Button from '@material-ui/core/Button';
-// import axios from "axios";
-// import {useState} from 'react';
 import WBScoket from './websocket/WebSocket';
 
 function App() {
 
-/*  const [randomCode, setRandomCode] = useState("");
-
-  function getRandomCode () {
-    axios.get("http://192.168.0.128:8080/GetRandomNumber")
-    .then(res => {
-      if (res.status === 200){
-        setRandomCode(res.data.randomCode);
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }*/
-
   function getCurrentImageFrame(){
     let AppHagtControlScreen;
     let ctx;
-    let img = new Image();
+    const img = new Image();
     img.onload = function(){
       AppHagtControlScreen.width = window.innerWidth;
       AppHagtControlScreen.height = window.innerHeight;
       ctx.drawImage(img,0,0,AppHagtControlScreen.width,AppHagtControlScreen.height);
     }
-    let webScoket = new WBScoket({
-      socketUrl: 'ws://192.168.0.128:8888/Connection?currentTime=' + new Date().getTime(),
+    const webScoket = new WBScoket({
+      socketUrl: 'ws://localhost:8888/Connection?currentTime=' + new Date().getTime(),
       timeout: 5000,
       socketMessage: (receive) => {
-        let bolbImage = receive.data;
+        const bolbImage = receive.data;
         window.URL.revokeObjectURL(img.src);
         img.src = window.URL.createObjectURL(bolbImage);
       },
@@ -69,12 +51,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <canvas id="AppHagtControlScreen" className="App-hagt-control-screen"></canvas>
-{/*        <img id="AppLogo" src={logo} className="App-logo" alt="logo"/>
-        <div >{randomCode}</div>
-        <br/>
-        <Button onClick={getRandomCode} variant="contained" color="primary" disableElevation>
-          生成号码
-        </Button>*/}
       </header>
     </div>
   );
